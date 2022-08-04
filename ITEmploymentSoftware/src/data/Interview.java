74a5ed9d201367a68c52bb9efb1fbb1b448fd898
SleepyN3ko <snowderemusic@gmail.com>
2022-08-04 12:46:21 +0800

    test

--- ITEmploymentSoftware/src/data/Interview.java
+++ ITEmploymentSoftware/src/data/Interview.java
@@ -1,9 +1,24 @@
 package data;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
 
 public class Interview {
-
+	private String InterviewID;
+	private String IntervieweeID;
+	private String StaffName;
+	private String Position;
+	private Date InterviewDate;
+	private String Time;
+	public Interview(String IntervieweeID,String StaffName,String Position, Date InterviewDate,String Time){
+		this.InterviewID = UUID.randomUUID().toString();
+		this.IntervieweeID = IntervieweeID;
+		this.StaffName = StaffName;
+		this.Position = Position;
+		this.InterviewDate = InterviewDate;
+		this.Time = Time;
+	}
+	
 }
